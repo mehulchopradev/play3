52efe058b1817932724459671eced46237d15613
stark <mehul.chopra.dev@gmail.com>
2017-10-27 22:27:45 +0530

    function to return year component fromt date

--- date-util.py
+++ date-util.py
@@ -1,3 +1,5 @@
 # a python module that will have all the functions with regards to date handling
 
 # function to return date in a particular format
+
+# function to return only the year component from the date
